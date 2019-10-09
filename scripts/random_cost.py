b0dac30d8b489d9119ccd4be7d45c231a7b7bfe7
Jack Kawell <jgkawell@outlook.com>
2019-10-09 17:34:40 -0600

    Renamed random cost and added in static cost

---
+++ scripts/random_cost.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from ompl.srv import CustomCost, CustomCostResponse
+import rospy
+import random
+
+def handle_random_cost(request):
+    cost = random.random()
+    rospy.loginfo("Returning cost: %f"%cost)
+    return cost
+
+def random_cost_server():
+    rospy.init_node('random_cost_server')
+    rospy.Service('custom_cost', CustomCost, handle_random_cost)
+    rospy.loginfo("Ready to compute random cost.")
+    rospy.spin()
+
+if __name__ == "__main__":
+    random_cost_server()
