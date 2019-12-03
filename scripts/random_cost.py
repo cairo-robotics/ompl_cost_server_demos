2a07c07b3a8595f41994425ced122a186e727dcb
Jack Kawell <jgkawell@outlook.com>
2019-12-03 10:15:39 -0600

    Cleaned up cost functions according to pep8

--- scripts/random_cost.py
+++ scripts/random_cost.py
@@ -4,16 +4,19 @@ from moveit_msgs.srv import CustomCost, CustomCostResponse
 import rospy
 import random
 
+
 def handle_random_cost(request):
     cost = random.random()
-    rospy.loginfo("Returning cost: %f"%cost)
+    rospy.loginfo("Returning cost: %f" % cost)
     return cost
+
 
 def random_cost_server():
     rospy.init_node('random_cost_server')
     rospy.Service('custom_cost', CustomCost, handle_random_cost)
     rospy.loginfo("Ready to compute random cost.")
     rospy.spin()
+
 
 if __name__ == "__main__":
     random_cost_server()
--- scripts/static_cost.py
+++ scripts/static_cost.py
@@ -3,16 +3,19 @@
 from moveit_msgs.srv import CustomCost, CustomCostResponse
 import rospy
 
+
 def handle_static_cost(request):
     cost = 0.1
-    rospy.loginfo("Returning cost: %f"%cost)
+    rospy.loginfo("Returning cost: %f" % cost)
     return cost
+
 
 def static_cost_server():
     rospy.init_node('static_cost_server')
     rospy.Service('custom_cost', CustomCost, handle_static_cost)
     rospy.loginfo("Ready to compute static cost.")
     rospy.spin()
+
 
 if __name__ == "__main__":
     static_cost_server()
